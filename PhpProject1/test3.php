4aa0e57b6a6d5971c1f5049e259c864582d7b975
marinsimeonov1 <martin_simeonov@abv.bg>
2014-11-04 14:36:40 +0200

    Update test3.php

--- PhpProject1/test3.php
+++ PhpProject1/test3.php
@@ -1,5 +1,5 @@
 <?php
-echo('14:35'):;
+echo('14:35');
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
